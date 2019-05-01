4908244c7b5965bd50717ec7bb34e9b7ea71a093
ryo.s <aqua.timez.saiko@gmail.com>
2019-05-02 01:55:57 +0900

    Initial Commit

---
+++ SNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by ryo shirai on 2019/05/02.
+//  Copyright © 2019 ryo shirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
